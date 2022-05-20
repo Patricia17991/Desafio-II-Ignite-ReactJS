ebbd21f8c90a5664fba9d61f33ac6d5ad426b80d
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-05-20 12:24:37 -0300

    Update SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,28 +1,35 @@
-import { GenreResponseProps } from "../@types/GenreResponseProps";
 import { Button } from "./Button";
 
-interface sidebarProps{
-  handleClickButton:(id:number)=>void;
+interface SideBarProps {
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>;
   selectedGenreId: number;
-  genres: GenreResponseProps[]
+  buttonClickCallback: (args: any) => void;
 }
-export function SideBar({handleClickButton, selectedGenreId, genres}:sidebarProps) {
 
+export function SideBar({
+  genres,
+  selectedGenreId,
+  buttonClickCallback
+}: SideBarProps) {
   return (
     <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
+      <span>Watch<p>Me</p></span>
 
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => buttonClickCallback(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
 
     </nav>
   )
